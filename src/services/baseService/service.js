497a91f35c665befc8a41d8b05b104c146d2a231
Vitor Leal <vitor_leal2201@hotmail.com>
2020-08-17 20:31:33 -0300

    Init project setup
    Signed-off-by: Vitor Leal <vitor_leal2201@hotmail.com>

---
+++ src/services/baseService/service.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const BaseUrl = '';
+
+const API = axios.create({
+  baseURL: BaseUrl,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
+
+API.interceptors.response.use(
+  (response) => {
+    if (
+      response.data &&
+      'success' in response.data &&
+      response.data.success === false
+    ) {
+      return Promise.reject(response.data);
+    }
+    return Promise.resolve(response.data);
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default API;
